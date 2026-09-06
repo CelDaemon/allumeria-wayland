a74dfc19654fd7115c254839b89a4e532dce1d1d
CelDaemon <celdaemon@voidgroup.net>
2026-09-06 02:30:09 +0200

    Initial testing

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,5 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SoLoud;
+using Steamworks;
 
 var soloud = new Soloud();
+
+
+SteamClient.Init(4000);
 Console.WriteLine("Hello, World!");
--- decompile.sh
+++ decompile.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 
+set -eu
+
+base_dir="$(dirname "$(realpath "$0")")"
+patch_dir="$base_dir/patch"
 game_dir="$1"
 
 allumeria_dll="$game_dir/Allumeria.dll"
 
+decomp_dir="$base_dir/Allumeria"
 
 if ! [ -f "$allumeria_dll" ]
 then
     echo "Could not find Allumeria.dll" >&2
     exit 1
 fi
+
+# rm -r "$decomp_dir"
+
+dotnet tool run ilspycmd -- --nested-directories -p -o "$base_dir/Allumeria" "$allumeria_dll"
+
+cp "$patch_dir/Allumeria.csproj" "$decomp_dir/Allumeria.csproj"
+
+cd "$decomp_dir"
+
+patch -p1 "$patch_dir/"*.patch
